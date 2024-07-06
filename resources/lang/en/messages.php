36e818cf1f93c39c4cf8ac50860574a5fe79299c
emnabouaziz <emna.bouaziz@esprit.tn>
2024-07-07 00:59:37 +0100

    crud_done

---
+++ resources/lang/en/messages.php
@@ -0,0 +1,16 @@
+<?php
+return [
+    "API_RESP_MSG_GET_SUCCESS"               => "Data fetched successfully.",
+    "API_RESP_MSG_UPLOAD_SUCCESS"            => "Upload operation finished.",
+    "API_RESP_MSG_POST_SUCCESS"              => "Item stored.",
+    "API_RESP_MSG_UPDATE_SUCCESS"            => "Item updated.",
+    "API_RESP_MSG_DELETE_SUCCESS"            => "Item deleted.",
+    "API_RESP_MSG_NOT_FOUND"                 => "Item(s) not found.",
+    "API_RESP_MSG_BAD_REQUEST"               => "Bad request.",
+    "API_RESP_MSG_EMPTY"                     => "No item.",
+    "API_RESP_MSG_CONFLICT"                  => "Item already exists.",
+    "API_RESP_MSG_CONFLICT_UNIQUE_NAME"      => "The name is already taken.",
+    "API_RESP_MSG_INTERNAL_SERVER_ERROR"     => "Internal server error",
+    "API_RESP_MSG_CANT_BE_DELETED"           => "This item can't be deteled.",
+    "API_RESP_MSG_RESTORE_SUCCESS"            => "Item restored.",
+];
